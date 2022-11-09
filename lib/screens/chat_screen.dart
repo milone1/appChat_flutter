8d6ecddec1145b03e49dff4b9ca4da9c3703fe3b
Erick Milan <erickmilan1002@gmail.com>
2022-11-08 22:42:13 -0500

    add button functionality

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,15 +1,141 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-class ChatScreen extends StatelessWidget {
-const ChatScreen({super.key});
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
 
   @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+  final _focusNode = FocusNode();
+  bool _estaEscribiendo = false;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Chat Screen'),
-     ),
-   );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(
+          child: Column(
+            children: const <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.blueAccent,
+                maxRadius: 14,
+                child: Text(
+                  'te',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 3,
+              ),
+              Text(
+                'Melizza Flores',
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        centerTitle: true,
+        elevation: 1,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                reverse: true,
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (_, i) => Text('$i'),
+              ),
+            ),
+            const Divider(
+              height: 1,
+            ),
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: _inputChat(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _inputChat() {
+    return SafeArea(
+        child: Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 8.0,
+      ),
+      child: Row(
+        children: [
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmit,
+              onChanged: (texto) {
+                // setState(() {
+                //   if (texto.length > 0) {
+                //     _estaEscribiendo = true;
+                //   } else {
+                //     _estaEscribiendo = false;
+                //   }
+                // });
+              },
+              decoration: const InputDecoration.collapsed(
+                hintText: 'Enviar Mensaje',
+              ),
+              focusNode: _focusNode,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: Platform.isIOS
+                ? CupertinoButton(
+                    onPressed: !_estaEscribiendo
+                        ? () => _handleSubmit(_textController.text.trim())
+                        : null,
+                    child: const Text('Send'),
+                  )
+                : Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                    child: const IconTheme(
+                      data: IconThemeData(color: Colors.blue),
+                      child: IconButton(
+                        
+                        highlightColor: Colors.transparent,
+                        splashColor: Colors.transparent,
+                        icon: Icon(
+                          Icons.send,
+                          color: Colors.blue,
+                        ),
+                        onPressed: null,
+                      ),
+                    ),
+                  ),
+          ),
+        ],
+      ),
+    ));
+  }
+
+  _handleSubmit(String text) {
+    print(text);
+    _focusNode.requestFocus();
+    _textController.clear();
+    setState(() {
+      _estaEscribiendo = false;
+    });
   }
 }
